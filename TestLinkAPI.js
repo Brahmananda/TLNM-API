79383f9cfdf8a9bbfce97a9fe2ef540e8ea45356
Brahmananda <brahma.sipu@gmail.com>
2014-07-24 14:32:00 +0530

    Revert "JSON object implemented"
    This reverts commit b2f9ac089e62e96e3b68e71526cabeb5c8ca5274.

--- TestLinkAPI.js
+++ TestLinkAPI.js
@@ -1,311 +1,166 @@
-var http = require('http');
+var http 		= require('http');
 var fs          = require('fs');
-var utilites = require('./Utilities');
-var xmlRequest	= require('./XmlRequest');
-var dom = require('xmldom').DOMParser;
+var utilites 	= require('./Utilities');
+var xmlRequest	= require('./xmlRequest');
+var xmlParser	= require('./xmlParser');
+var GTPBN_Components = require('./Components/getTestPlanByName-components');
+var GTC_Components = require('./Components/getTestCase-components');
+var GBBP_Components = require('./Components/getBuildByPlan-components');
+var GP_Components = require('./Components/getProjects-components');
 
+
+
+
+
+var xpath 	= require('xpath');
+var dom 	= require('xmldom').DOMParser;
+  
 module.exports=TestLinkApi=function (devkey,rpcUrl){
-this.TestUrl=rpcUrl;
-this.devkey=devkey;	
-}
+	this.TestUrl=rpcUrl;
+	this.devkey=devkey;					 
+	}
 
-/*To Get the Available Builds Created for Passing TestPlan ID*/
-TestLinkApi.prototype.getBuildsForTestPlan=function(testPlanId,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getBuildsForTestPlan(this.devkey,testPlanId);
-utilites.postrequest(post,body,function(response){
-var buildsfortestplan=utilites.getJsObjectByXmlResponse(response);
-callback(buildsfortestplan);
-});
+TestLinkApi.prototype.hello=function(){
+	console.log(this.TestUrl);
+	 };
+	 
+	 /*To Get the Available Builds Created for Passing TestPlan ID*/
+TestLinkApi.prototype.getBuildsForTestPlan=function(TestPlanId,callback){
+		var post=utilites.postCompose(this.TestUrl);
+		var body=xmlRequest.getBuildsForTestPlan(this.devkey,TestPlanId);
+		utilites.postrequest(post,body,function(response){
+		var doc = new dom().parseFromString(response) 
+		var objects= xpath.select("//struct",doc);
+		var Builds=[];
+		objects.map(function(object){
+		Builds.push(new GBBP_Components(object.toString()).getCacheObject());
+		});
+		callback(Builds);
+		});
+	 };
+	
+	/*To Get DevKey*/
+TestLinkApi.prototype.getDevKey=function(){
+	return this.devkey
+	};
+	
+	/*To Get the Created Project Names*/
+TestLinkApi.prototype.getProjects=function(callback){
+	var post=utilites.postCompose(this.TestUrl);
+	var body=xmlRequest.getProjects(this.devkey);
+	utilites.postrequest(post,body,function(response){
+		//console.log(response);
+		var doc = new dom().parseFromString(response) 
+		var objects= xpath.select("//struct",doc);
+		var Projects=[];
+		objects.map(function(object){
+		if(object.toString().indexOf("<name>name</name>") > -1){
+		var xmlvalue=object.toString();
+		Projects.push(new GP_Components(xmlvalue));
+		}
+		});
+		callback(Projects);
+		});
+	};
+	
+	/*To Get the Available TestPlans for the Passing Project ID*/
+TestLinkApi.prototype.getProjectTestPlans=function(ProjectId,callback){
+	var post=utilites.postCompose(this.TestUrl);
+	var body=xmlRequest.getProjectTestPlans(this.devkey,ProjectId);
+	utilites.postrequest(post,body,function(response){
+		//console.log(response);
+		var doc = new dom().parseFromString(response) 
+		var objects= xpath.select("//struct",doc);
+		var testPlans=[];
+		objects.map(function(object){
+		testPlans.push(new GBBP_Components(object.toString()));
+		});
+		callback(testPlans);
+		});
+};
+	
+	/*To Get the Available TestPlan Name*/
+TestLinkApi.prototype.getTestPlanByName=function(TestProjectName,TestPlanName,callback){
+	var post=utilites.postCompose(this.TestUrl);
+	var body=xmlRequest.getTestPlanByName(this.devkey,TestProjectName,TestPlanName);
+	utilites.postrequest(post,body,function(response){
+	var gtpbn_exts=new GTPBN_Components(response);
+	callback(gtpbn_exts);
+		});
+};
+	/*To Get the Available TestSuite Names for the Passing TestPlan ID*/
+TestLinkApi.prototype.getTestSuitesForTestPlan=function(TestPlanId,callback){
+	var post=utilites.postCompose(this.TestUrl);
+	var body=xmlRequest.getTestSuitesForTestPlan(this.devkey,TestPlanId);
+	utilites.postrequest(post,body,function(response){
+	var Suite_Plan= xmlParser.getTestSuitesForTestPlan(response);
+	callback(Suite_Plan);
+		});
+};
+	
+	/*To Get the Available Platforms Names for the Passing TestPlan ID*/
+TestLinkApi.prototype.getTestPlanPlatforms=function(TestPlanId,callback){
+	var post=utilites.postCompose(this.TestUrl);
+	var body=xmlRequest.getTestPlanPlatforms(this.devkey,TestPlanId);
+	utilites.postrequest(post,body,function(response){
+	var Plan_Platform= xmlParser.getTestPlanPlatforms(response);
+	callback(Plan_Platform);
+	});
 };
 
-TestLinkApi.prototype.getProjectPlatforms=function(testprojectid,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getProjectPlatforms(this.devkey,testprojectid);
-utilites.postrequest(post,body,function(response){
-var projectPlatforms=utilites.getJsObjectByXmlResponse(response);
-callback(projectPlatforms);
-});
+	/*To Get the URL*/
+TestLinkApi.prototype.getUrl=function(){
+	return this.TestUrl
+};
+	
+	/*To Get the TestCase Names for the Passing TestCaseID and TestCase External ID*/
+TestLinkApi.prototype.getTestCase=function(TestCaseExtId,callback){
+	var post=utilites.postCompose(this.TestUrl);
+	var body=xmlRequest.getTestCase(this.devkey,TestCaseExtId);
+	utilites.postrequest(post,body,function(response){
+	//console.log(response);
+	var gtc_exts=new GTC_Components(response);
+	callback(gtc_exts);
+	});
+};
+	
+	/*To Get the TestCase Names for the Passing TestProject ID and TestSuite ID*/
+TestLinkApi.prototype.getTestCaseNames=function(TestProjectId,TestSuiteId,callback){
+	var post=utilites.postCompose(this.TestUrl);
+	var body=xmlRequest.getTestCaseNames(this.devkey,TestProjectId,TestSuiteId);
+	utilites.postrequest(post,body,function(response){
+	var TestCaseNames= xmlParser.getTestCaseNames(response);
+	callback(TestCaseNames);
+		});
+};	
+
+   /* Upload the attachment to test case  */
+TestLinkApi.prototype.uploadTestCaseAttachment=function(testcaseid,filename,uploadpath){
+	var post=utilites.postCompose(this.TestUrl);
+	var bitmap = fs.readFileSync(uploadpath+filename);
+	var content=new Buffer(bitmap).toString('base64');
+	//console.log(content);
+	var body=xmlRequest.uploadTestCaseAttachment(this.devkey,testcaseid,"text/plain",filename,content);
+	utilites.postrequest(post,body,function(response){}); 
 }; 
 
-/*To Get the Available TestPlans for the Passing Project ID*/
-TestLinkApi.prototype.getProjectTestPlans=function(projectId,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getProjectTestPlans(this.devkey,projectId);
-utilites.postrequest(post,body,function(response){
-var projectPlans=utilites.getJsObjectByXmlResponse(response);
-callback(projectPlans);
-});
-};
-
-/*To Get the Created Project Names*/
-TestLinkApi.prototype.getProjects=function(callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getProjects(this.devkey);
-utilites.postrequest(post,body,function(response){
-var projects=utilites.getJsObjectByXmlResponse(response);
-callback(projects);
-});
-};
-
-/*To Get the TestCase Names for the Passing TestCaseID and TestCase External ID*/
-TestLinkApi.prototype.getTestCase=function(testCaseExtId,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getTestCase(this.devkey,testCaseExtId);
-utilites.postrequest(post,body,function(response){
-var testcase=utilites.getJsObjectByXmlResponse(response);
-callback(testcase);
-});
-};
 
 TestLinkApi.prototype.getTestCaseAttachments=function(testcaseexternalid,downloadpath){
-var post=utilites.postCompose(this.TestUrl);	
-var body=xmlRequest.getTestCaseAttachments(this.devkey,testcaseexternalid);
-utilites.postrequest(post,body,function(response){
-var attachments=utilites.getJsObjectByXmlResponse(response);
-var component_names=jsonPath.eval(attachments, "$..name");
-var component_contents=jsonPath.eval(attachments, "$..content");
-var index=0;
-component_contents.map(function(content){
-var buf = new Buffer(content, 'base64'); 
-fs.writeFile(downloadpath+component_names[index++],buf, function (err) {
-if (err) throw err;
-console.log('File Has Been Downloaded');
-});
-});
-});
+	var post=utilites.postCompose(this.TestUrl);	
+	var body=xmlRequest.getTestCaseAttachments(this.devkey,testcaseexternalid);
+	utilites.postrequest(post,body,function(response){
+	var components=new Components(response);
+	var names=components.getNames(); //xmlParser.getNames(response);
+	var contents=components.getContent();//xmlParser.getContent(response);
+	var index=0;
+		contents.map( function(content){
+		var buf = new Buffer(content, 'base64'); 
+			fs.writeFile(downloadpath+names[index++],buf, function (err) {
+			if (err) throw err;
+			console.log(' file been downloaded !!');
+			});
+		});
+	});
 };
-
-TestLinkApi.prototype.getTestCaseCustomFieldDesignValue=function(testprojectid,testcaseexternalid,customfieldname,details,version,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getTestCaseCustomFieldDesignValue(this.devkey,testprojectid,testcaseexternalid,customfieldname,"details",1);
-utilites.postrequest(post,body,function(response){
-var customFieldValue=utilites.getJsObjectByXmlResponse(response);
-callback(customFieldValue);
-});
-};
-
-TestLinkApi.prototype.getTestCasesForTestPlan=function(testplanid,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getTestCasesForTestPlan(this.devkey,testplanid);
-utilites.postrequest(post,body,function(response){
-   var testCases=utilites.getJsObjectByXmlResponse(response);
-callback(testCases);
-});
-};
-
-TestLinkApi.prototype.getTestCasesForTestSuite=function(testprojectid,testsuiteid,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getTestCasesForTestSuite(this.devkey,testprojectid,testsuiteid);
-utilites.postrequest(post,body,function(response){
-var getsuites=utilites.getJsObjectByXmlResponse(response);
-   callback(getsuites);	
-});
-};
-
-TestLinkApi.prototype.getTestCaseIDByName=function(testcasename,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getTestCaseIDByName(this.devkey,testcasename);
-utilites.postrequest(post,body,function(response){
-console.log(response);
-var testcaseid=utilites.getJsObjectByXmlResponse(response);
-   callback(testcaseid);	
-});
-
-
-};
-
-/*To Get the Available TestPlan Name*/
-TestLinkApi.prototype.getTestPlanByName=function(testProjectName,testPlanName,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getTestPlanByName(this.devkey,testProjectName,testPlanName);
-utilites.postrequest(post,body,function(response){
-var planName=utilites.getJsObjectByXmlResponse(response);
-callback(planName);
-});
-};
-
-/*To Get the Available Platforms Names for the Passing TestPlan ID*/
-TestLinkApi.prototype.getTestPlanPlatforms=function(testPlanId,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getTestPlanPlatforms(this.devkey,testPlanId);
-utilites.postrequest(post,body,function(response){
-var platforms=utilites.getJsObjectByXmlResponse(response);
-callback(platforms);
-});
-};
-
-TestLinkApi.prototype.getTestProjectByName=function(testProjectName,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getTestProjectByName(this.devkey,testProjectName);
-utilites.postrequest(post,body,function(response){
-var projectName=utilites.getJsObjectByXmlResponse(response);
-callback(projectName);
-});
-};
-
-/*To Get the Available TestSuite Names for the Passing TestPlan ID*/
-TestLinkApi.prototype.getTestSuitesForTestPlan=function(testPlanId,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getTestSuitesForTestPlan(this.devkey,testPlanId);
-utilites.postrequest(post,body,function(response){
-var testSuites=utilites.getJsObjectByXmlResponse(response);
-callback(testSuites);
-});
-};
-
-TestLinkApi.prototype.getTestSuitesForTestSuite=function(testsuiteid,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getTestSuitesForTestSuite(this.devkey,testsuiteid);
-utilites.postrequest(post,body,function(response){
-console.log(response);
-var testsuites=utilites.getJsObjectByXmlResponse(response);
-callback(testsuites);
-});
-}; 
-
-TestLinkApi.prototype.getUserByID=function(userid,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getUserByID(this.devkey,userid);
-utilites.postrequest(post,body,function(response){
-var userByID=utilites.getJsObjectByXmlResponse(response);
-callback(userByID);
-});
-};
-
-TestLinkApi.prototype.getUserByLogin=function(user,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getUserByLogin(this.devkey,user);
-utilites.postrequest(post,body,function(response){
-var userByLogin=utilites.getJsObjectByXmlResponse(response);
-callback(userByLogin);
-});
-};
-
-TestLinkApi.prototype.uploadTestCaseAttachment=function(testcaseid,filename,uploadpath){
-var post=utilites.postCompose(this.TestUrl);
-var bitmap = fs.readFileSync(uploadpath+filename);
-var content=new Buffer(bitmap).toString('base64');
-var body=xmlRequest.uploadTestCaseAttachment(this.devkey,testcaseid,"text/plain",filename,content);
-utilites.postrequest(post,body,function(response){}); 
-};
-
-
-
-/*To Get DevKey*/
-TestLinkApi.prototype.getDevKey=function(){
-return this.devkey
-};
-
-/*To Get the URL*/
-TestLinkApi.prototype.getUrl=function(){
-return this.TestUrl
-};
-
-
-TestLinkApi.prototype.getExecCountersByBuild=function(testPlanId,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getExecCountersByBuild(this.devkey,testPlanId);
-utilites.postrequest(post,body,function(response){
-var execCounters=utilites.getJsObjectByXmlResponse(response);
-callback(execCounters);
-});
-};
-
-TestLinkApi.prototype.getFirstLevelTestSuitesForTestProject=function(testProjectId,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getFirstLevelTestSuitesForTestProject(this.devkey,testProjectId);
-utilites.postrequest(post,body,function(response){
-var firstLevelSuite=utilites.getJsObjectByXmlResponse(response);
-callback(firstLevelSuite);
-});
-};
-
-TestLinkApi.prototype.getFullPath=function(nodeId,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getFullPath(this.devkey,nodeId);
-utilites.postrequest(post,body,function(response){
-var fullpath=utilites.getJsObjectByXmlResponse(response);
-callback(fullpath);
-});
-};
-
-TestLinkApi.prototype.getLastExecutionResult=function(testplanid,testcaseexternalid,callback){
-   var post=utilites.postCompose(this.TestUrl);
-   var body=xmlRequest.getLastExecutionResult(this.devkey,testplanid,testcaseexternalid);
-   utilites.postrequest(post,body,function(response){
-console.log(response);
-   var lastresult=utilites.getJsObjectByXmlResponse(response);
-   callback(lastresult);
-       });
-};
-
-TestLinkApi.prototype.removePlatformFromTestPlan=function(testplanid,platformname,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.removePlatformFromTestPlan(this.devkey,testplanid,platformname);
-utilites.postrequest(post,body,function(response){
-var removeplatform=utilites.getJsObjectByXmlResponse(response);
-callback(removeplatform);
-});
-};
-
-TestLinkApi.prototype.reportTCResult=function(testplanid,testcaseexternalid,buildid,notes,status,platformname,user,bugid,callback){
-   var post=utilites.postCompose(this.TestUrl);
-   var body=xmlRequest.reportTCResult(this.devkey,testplanid,testcaseexternalid,buildid,notes,status,platformname,"","admin");
-   utilites.postrequest(post,body,function(response){
-   var tcresult=utilites.getJsObjectByXmlResponse(response);
-   callback(tcresult);
-       });
-};
-
-TestLinkApi.prototype.reportTCResultOverwrite=function(testplanid,testcaseexternalid,buildid,notes,status,platformname,overwrite,user,bugid,callback){
-   var post=utilites.postCompose(this.TestUrl);
-   var body=xmlRequest.reportTCResultOverwrite(this.devkey,testplanid,testcaseexternalid,buildid,notes,status,platformname,overwrite,"admin");
-   utilites.postrequest(post,body,function(response){
-   var resultoverwrite=utilites.getJsObjectByXmlResponse(response);
-   callback(resultoverwrite);
-       });
-};
-
-TestLinkApi.prototype.setTestCaseExecutionType=function(testprojectid,testcaseexternalid,version,executiontype,callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.setTestCaseExecutionType(this.devkey,testprojectid,testcaseexternalid,version,executiontype);
-utilites.postrequest(post,body,function(response){
-var removeplatform=utilites.getJsObjectByXmlResponse(response);
-callback(removeplatform);
-});
-};
-
-
-TestLinkApi.prototype.getTestLinkVersion=function(callback){
-var post=utilites.postCompose(this.TestUrl);
-var body=xmlRequest.getTestLinkVersion(this.devkey);
-utilites.postrequest(post,body,function(response){
-console.log(response);
-var testlinkversion=utilites.getJsObjectByXmlResponse(response);
-callback(testlinkversion);
-});
-};
-
-TestLinkApi.prototype.updateTestCase=function(testcaseexternalid,version,summary,callback){
-   var post=utilites.postCompose(this.TestUrl);
-   var body=xmlRequest.updateTestCase(this.devkey,testcaseexternalid,version,summary);
-   utilites.postrequest(post,body,function(response){
-   var updatetc=utilites.getJsObjectByXmlResponse(response);
-   callback(updatetc);
-       });
-};
-
- TestLinkApi.prototype.updateTestCaseCustomFieldDesignValue=function(testprojectid,testcaseexternalid,version,custonfiledname,custonfiledvalue,callback){
-   var post=utilites.postCompose(this.TestUrl);
-   var body=xmlRequest.updateTestCaseCustomFieldDesignValue(this.devkey,testprojectid,testcaseexternalid,version,custonfiledname,custonfiledvalue);
-   utilites.postrequest(post,body,function(response){
-   var customfiled=utilites.getJsObjectByXmlResponse(response);
-   callback(customfiled);
-       });
-};
-
-
-
 
 
--- Utilities.js
+++ Utilities.js
@@ -1,11 +1,12 @@
 var http = require('http');
-var xpath = require('xpath');
-var dom = require('xmldom').DOMParser;
-var SimpleConvertXML = require('simpleConvert-XML');
+var xpath 	= require('xpath');
+var dom 	= require('xmldom').DOMParser;
 
 module.exports={
 
-postCompose:function(url){
+postCompose:function(url)
+{
+console.log(url);
 var re = /http:\/\/([^:]*):([^\/]*)([^$]*)/
 var tokens = url.match(re);
 var theWholeUrl = tokens[0];
@@ -14,67 +15,26 @@ var port = tokens[2];
 var path = tokens[3];
 
 return {
-   host:host,
-   path:path,
-   port:port,
-   method: "POST",
-   headers: {
-       'Cookie': "cookie",
-       'Content-Type': 'text/xml',
-   }};
+    host:host,
+    path:path,
+    port:port,
+    method: "POST",
+    headers: {
+        'Cookie': "cookie",
+        'Content-Type': 'text/xml',
+    }
+};
 },
 postrequest:function(post,body,callback){
-var req = http.request( post, function( res ){
-  var buffer = "";
-  res.on( "data", function( data ) { buffer = buffer + data; } );
-  res.on( "end", function( data ) { 
-callback(buffer);
-  });
+var req = http.request( post, function( res )    {
+   var buffer = "";
+   res.on( "data", function( data ) { buffer = buffer + data; } );
+   res.on( "end", function( data ) { 
+	callback(buffer);
+   } );
 });
 req.write( body );
 req.end();
-},
-
-getJsObjectByXmlResponse:function(response){
-
-   var parser=new dom().parseFromString(response);
-   if(xpath.select("//data",parser).length>0)
-   response=xpath.select("//data",parser)[0].toString();
-   parser=new dom().parseFromString(response);
-   if(xpath.select("//param",parser).length>0)
-   response=xpath.select("//param",parser)[0].toString();
-   var changeAttrToTag=/<member><name>(.*)<\/name><value><(string|int)>((.|[\r\n])*?)<\/\2><\/value><\/member>/;
-   var nullyfy=/<member><name>.*?<string\/>.*?<\/member>/;
-if(nullyfy.test(response))
-   response=response.replace(/<member><name>.*?<string\/>.*?<\/member>/g,'');
-   var flag=changeAttrToTag.test(response);
-   var changedTagResponse="";
-   if(flag)
-   {
-       response=response.replace(/<member><name>(.*)<\/name><value><(string|int)>((.|[\r\n])*?)<\/\2><\/value><\/member>/g, "<$1>$3</$1>");
-       var recusiveRegx=/<member><name>(.*)?<\/name><value><struct>(((.|[\r\n])(?!<struct>))*?)<\/struct><\/value><\/member>/;
-       var recursiveflag=recusiveRegx.test(response);
-           while(recursiveflag){
-             response=response.replace(/<member><name>(.*)?<\/name><value><struct>(((.|[\r\n])(?!<struct>))*?)<\/struct><\/value><\/member>/, "<_$1>$2</_$1>");
-             recursiveflag=recusiveRegx.test(response);
-           }
-       response=response.replace(/<member><name>(.*)?<\/name><value><struct>(((.|[\r\n])(?!<struct>))*?)<\/struct><\/value><\/member>/, "<$1>$2</$1>");
-   }
-
-var xmlNode = new dom().parseFromString(response);
-var json=SimpleConvertXML.getXMLAsObj(xmlNode);
-   var returnObject;
-   if(json.data)
-   {
-   returnObject=json.data.value;
-   }else{
-     if(json.param)
-     {
-       returnObject=json.param.value;
-     }
-   }
-
-return returnObject;
 }
 
 
